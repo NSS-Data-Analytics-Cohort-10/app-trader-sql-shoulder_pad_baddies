891284e41fe1d799cbca22190156c1349f0b9ec2
antoinetteanderson <antoinetteanderson88@gmail.com>
2023-10-17 20:30:41 -0500

    added a query about top category

--- scripts/app-trader-sql-shoulder-pad_baddies-antoinette-anderson.sql
+++ scripts/app-trader-sql-shoulder-pad_baddies-antoinette-anderson.sql
@@ -1,6 +1,15 @@
-SELECT name, a. price, a. rating, category, p.price , p.rating
+-- Filtering apps based on price, rating, category, and whether they are both in play and app store
+SELECT name, a. price, a. rating, category, p.price , p.rating, genres
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 USING(name)
-GROUP BY name, a.price, a.rating, category, p.price, p.rating
+GROUP BY name, a.price, a.rating, category, p.price, p.rating, genres
 ORDER BY a.rating*p.rating/2 DESC;
+
+--- Top category for the apps
+SELECT name, category, p. rating, a.rating
+FROM play_store_apps AS p
+INNER JOIN app_store_apps AS a
+USING (name)
+ORDER BY category DESC;
+
