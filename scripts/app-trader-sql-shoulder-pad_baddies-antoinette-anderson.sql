a322b46d6ca53d11eb84232f55ade44a63b1e460
antoinetteanderson <antoinetteanderson88@gmail.com>
2023-10-17 21:03:42 -0500

    adding notes first before working on large query

--- scripts/app-trader-sql-shoulder-pad_baddies-antoinette-anderson.sql
+++ scripts/app-trader-sql-shoulder-pad_baddies-antoinette-anderson.sql
@@ -7,9 +7,25 @@ GROUP BY name, a.price, a.rating, category, p.price, p.rating, genres
 ORDER BY a.rating*p.rating/2 DESC;
 
 --- Top category for the apps
-SELECT name, category, p. rating, a.rating
+SELECT name, category, p. rating as play_rating, a.rating as app_rating
 FROM play_store_apps AS p
 INNER JOIN app_store_apps AS a
 USING (name)
 ORDER BY category DESC;
+
+-- Working on deliverables a (reccomendataions)
+-- What the app trader is looking for:
+-- free to $1.00 is 10,000
+-- it is an exta 10k by the dollar
+
+
+
+SELECT DISTINCT
+
+
+
+
+
+
+
 
