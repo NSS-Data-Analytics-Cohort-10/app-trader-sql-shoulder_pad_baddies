13f5ac24d90d31b60e94739af51efb9a9f6e5b39
SullivanAV <sullivan.avd@gmail.com>
2023-10-17 20:44:13 -0500

    still working

--- app_trader.sql
+++ app_trader.sql
@@ -1,12 +1,19 @@
-SELECT a1.name, p1.name, (a1.rating*p1.rating)/2 AS avg_rating, p1.price
+SELECT a1.name, p1.name, (a1.rating+p1.rating)/2 AS avg_rating, p1.price
 FROM app_store_apps AS a1
 INNER JOIN play_store_apps AS p1
 USING (name)
 ORDER BY avg_rating DESC, p1.price ASC
 
-SELECT a1.name, (AVG(a1.rating)*AVG(p1.rating)/2) AS avg_rating
-FROM app_store_apps AS a1
-INNER JOIN play_store_apps AS p1
+SELECT a.name, (AVG(a.rating)+AVG(p.rating)/2) AS avg_rating
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
 USING (name)
-GROUP BY a1.name
+GROUP BY a.name
 ORDER BY avg_rating DESC
+
+SELECT a.name, p.name, a.rating, p.rating
+FROM app_store_apps AS a
+FULL OUTER JOIN play_store_apps AS p
+USING (name)
+WHERE a.rating = 5 OR p.rating=5
+ORDER by a.rating DESC
