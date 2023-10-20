45892170c09c1743c467ca3ec57331d108bb6f39
SullivanAV <sullivan.avd@gmail.com>
2023-10-19 19:39:11 -0500

    finding out store

--- app_trader.sql
+++ app_trader.sql
@@ -17,3 +17,31 @@ FULL OUTER JOIN play_store_apps AS p
 USING (name)
 WHERE a.rating = 5 OR p.rating=5
 ORDER by a.rating DESC
+
+SELECT *
+FROM app_store_apps
+
+SELECT *
+FROM play_store_apps
+
+SELECT a.name, p.name,
+ROUND(a.rating+p.rating),0/2 AS rating_both
+FROM app_store_apps AS a
+FULL OUTER JOIN play_store_apps AS p
+USING (name)
+GROUP BY a.name, p.name
+ORDER BY rating_both DESC
+
+SELECT a.name, p.name, 
+CASE WHEN a.name is NOT NULL and p.name IS NULL THEN 'app'
+WHEN p.name is NOT NULL AND a.name IS NULL THEN 'play'
+WHEN a.name IS NOT NULL AND p.name IS NOT NULL THEN 'both'
+END AS store
+FROM app_store_apps AS a
+FULL OUTER JOIN play_store_apps AS p
+USING (name)
+
+
+
+
+
